3201ea9b19a75dc12f4d0149ddef987d12fb1116
anargiris <anargiriss@hotmail.com>
2024-02-03 19:36:47 +0200

    add code highlight, fix some bugs

--- app/blog/[slug]/page.js
+++ app/blog/[slug]/page.js
@@ -1,9 +1,16 @@
 import fs from "fs";
 import path from "path";
-import matter from "gray-matter";
-
+import rehypeHighlight from "rehype-highlight";
 import { MDXRemote } from "next-mdx-remote/rsc";
 import { getBlogPost } from "@/lib/blog_utils";
+import "@/styles/highlight-js/github-dark.css";
+
+const options = {
+  mdxOptions: {
+    remarkPlugins: [],
+    rehypePlugins: [rehypeHighlight],
+  },
+};
 
 export async function generateStaticParams() {
   const files = fs.readdirSync(path.join("blog_posts"));
@@ -29,9 +36,9 @@ export default function Page({ params }) {
   return (
     <div>
       Hello from the blog post page.
-      <div>
-        <MDXRemote source={props.content} />
-      </div>
+      <article className="prose prose-sm md:prose-base lg:prose-lg dark:prose-invert mx-auto ">
+        <MDXRemote source={props.content} options={options} />
+      </article>
     </div>
   );
 }
